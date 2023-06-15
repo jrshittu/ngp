23dc1115a34cc9366762bb23d6839c35dd4c589e
jrshittu <jrshittu@users.noreply.github.com>
2023-06-15 01:51:25 +0100

    Added Job Listing Teachers CPD News and events

--- mail/contact.js
+++ mail/contact.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+    $("#HireForm input, #HireForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
         },
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "Hire.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -32,7 +32,7 @@ $(function () {
                             .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
                             .append('</div>');
-                    $('#contactForm').trigger("reset");
+                    $('#HireForm').trigger("reset");
                 },
                 error: function () {
                     $('#success').html("<div class='alert alert-danger'>");
@@ -40,7 +40,7 @@ $(function () {
                             .append("</button>");
                     $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
                     $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
+                    $('#HireForm').trigger("reset");
                 },
                 complete: function () {
                     setTimeout(function () {
--- mail/contact.php
+++ mail/contact.php
@@ -11,7 +11,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 
 $to = "info@example.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$body = "You have received a new message from your website Hire form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
